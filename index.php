2757109e1df7d562f7eef0d3d1de3d750a986517
root <courdheadman@gmail.com>
2018-02-23 19:51:55 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Shop Time</title>
+    <link rel="stylesheet" href="./styles/style.css" media="all">
+  </head>
+  <body>
+    <div class="main_wrapper">
+      <div class="">
+        header
+      </div>
+      <div class="">
+        menubar
+      </div>
+      <div class="">
+        sidebar
+      </div>
+      <div class="">
+        content
+      </div>
+      <div class="">
+        footer
+      </div>
+    </div>
+  </body>
+</html>
